b6ea45b1a9ef00ff2819b66e9076ba54acb3e421
Scott Allen <sallen1851@gmail.com>
2019-04-27 16:08:12 -0400

    t29

--- js/trek.js
+++ js/trek.js
@@ -1,3 +1,12 @@
+/*! jQuery v3.3.1 | (c) JS Foundation and other contributors | jquery.org/license */
+!function(e, t) {
+    "use strict";
+    "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
+        if (!e.document)
+            throw new Error("jQuery requires a window with a document");
+        return t(e)
+    }
+
 /* define: window, document */
 'use strict';
 
